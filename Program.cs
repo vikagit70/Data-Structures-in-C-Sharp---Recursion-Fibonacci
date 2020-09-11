1e5191ca764d1afe02d7e5e48409104132cfedf8
vikagit70 <54568566+vikagit70@users.noreply.github.com>
2020-09-10 22:52:43 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabonacci
+{
+    class Program
+    {
+        static void Fabonacci (int n1, int n2)
+        {
+            //at my discretion I added a limit for n2 value
+            if(n2 > 1000)
+            {
+                return;
+            } else
+            {
+                int sum = n1 + n2;
+                Console.WriteLine(sum);
+                Fabonacci(n2, sum);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Fabonacci(0, 1);
+            Console.WriteLine("The End");
+        }
+    }
+}
